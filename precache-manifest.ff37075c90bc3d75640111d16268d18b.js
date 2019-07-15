81663e773d6f7accb5fa504e962db995ac03473a
David Chorvinsky <firekenace@gmail.com>
2019-07-15 12:16:31 -0400

    Updates

---
+++ precache-manifest.ff37075c90bc3d75640111d16268d18b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e435aa85cf1572100f9d42f8015c172f",
+    "url": "/auctioneer-bot-website/index.html"
+  },
+  {
+    "revision": "9710df04359a396aea6b",
+    "url": "/auctioneer-bot-website/static/css/main.5ff99041.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/auctioneer-bot-website/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "9710df04359a396aea6b",
+    "url": "/auctioneer-bot-website/static/js/main.3889ebe2.chunk.js"
+  },
+  {
+    "revision": "8b1fdce318a767b0b1e2",
+    "url": "/auctioneer-bot-website/static/js/runtime~main.99ce1989.js"
+  }
+]);
