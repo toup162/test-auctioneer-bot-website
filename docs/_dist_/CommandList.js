391f41209a416a320ee23a90ca7095ef7448150c
David Chorvinsky <d.chorvinsky@gmail.com>
2020-11-25 21:06:42 -0500

    redeploy

--- docs/_dist_/CommandList.js
+++ docs/_dist_/CommandList.js
@@ -15,7 +15,7 @@ import{SvelteComponent as n,attr as a,detach as r,element as d,init as c,insert 
                 <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row"><div class="text-3xl flex-shrink-0 w-24 h-24 bg-gray-800 text-purple-400 rounded-full inline-flex items-center justify-center">!p</div> 
                     <div class="flex-grow sm:pl-6 mt-6 sm:mt-0"><h2 class="font-medium title-font text-white mb-1 text-xl">Item Stats Instantly</h2> 
                         <p class="leading-relaxed">Get key stats and information on any item on the Auction House.</p></div></div></div> 
-            <div class="flex relative lg:pl-2 pb-20 sm:items-center w-full"><div class="h-full w-6 absolute inset-0 flex items-center justify-center hidden lg:flex"><div class="h-full w-1 bg-gray-800 pointer-events-none"></div></div> 
+            <div class="flex relative lg:pl-10 pb-20 sm:items-center w-full"><div class="h-full w-6 absolute inset-0 flex items-center justify-center hidden lg:flex"><div class="h-full w-1 bg-gray-800 pointer-events-none"></div></div> 
                 <img src="!p.png" alt="example of !p call for akunda&#39;s bite to show the variety of information provided." class="md:pl-10 max-h-64"></div> 
             <div class="flex relative pl-2 pb-10 sm:items-center"><div class="h-full w-6 absolute inset-0 flex items-center justify-center hidden lg:flex"><div class="h-full w-1 bg-gray-800 pointer-events-none"></div></div> 
                 <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row"><div class="text-3xl flex-shrink-0 w-24 h-24 bg-gray-800 text-purple-400 rounded-full inline-flex items-center justify-center">!bg+</div> 
@@ -24,5 +24,5 @@ import{SvelteComponent as n,attr as a,detach as r,element as d,init as c,insert 
                             item&#39;s
                             recipes and so on.</p> 
                         <p class="italic">TLDR; know everything there is to know.</p></div></div></div> 
-            <div class="flex relative lg:pl-2 pb-20 sm:items-center w-full"><div class="h-full w-6 absolute inset-0 flex items-center justify-center hidden lg:flex"><div class="h-full w-1 bg-gray-800 pointer-events-none"></div></div> 
+            <div class="flex relative lg:pl-10 pb-20 sm:items-center w-full"><div class="h-full w-6 absolute inset-0 flex items-center justify-center hidden lg:flex"><div class="h-full w-1 bg-gray-800 pointer-events-none"></div></div> 
                 <img src="bg+.png" alt="Example of !bg+ call for bountiful captain&#39;s feast specifically showing multi-level recipe behavior." class="md:pl-10 max-h-64"></div></div></div>`,a(e,"class","text-gray-500 bg-gray-900 body-font pt-8")},m(t,i){o(t,e,i)},p:l,i:l,o:l,d(t){t&&r(e)}}}class x extends n{constructor(s){super();c(this,s,null,f,m,{})}}export default x;
