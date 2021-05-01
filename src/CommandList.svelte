d4809992b605db0e20c1603ec3bd93bb836dbc41
David Chorvinsky <d.chorvinsky@gmail.com>
2021-04-30 22:00:37 -0400

    updates for rank and news

--- src/CommandList.svelte
+++ src/CommandList.svelte
@@ -19,7 +19,7 @@
                 </div>
             </div>
             <div class="flex relative pl-2 pb-20 sm:items-center">
-               <div class="h-full w-6 absolute inset-0 flex items-center justify-center hidden lg:flex">
+                <div class="h-full w-6 absolute inset-0 flex items-center justify-center hidden lg:flex">
                     <div class="h-full w-1 bg-gray-800 pointer-events-none"></div>
                 </div>
                 <div class="flex-grow md:pl-8 flex sm:items-center items-start flex-col sm:flex-row">
@@ -28,15 +28,21 @@
                     </div>
                     <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
                         <h2 class="font-medium title-font text-white mb-1 text-xl">Pick a Realm, any Realm</h2>
-                        <p class="leading-relaxed">Simply type !r &#123;realm&#125; and hit enter. For example:
+                        <p class="leading-relaxed">Simply type !r &#123;realm&#125; and hit enter. A few examples:
+                            <br>
                             <code class="text-purple-300">!r arthas</code> or <code class="text-purple-300">!r
-                                arthas-eu</code>.
+                                arthas-us</code> for Arthas on US region.
+                            <br>
+                            <code class="text-purple-300">!r moon guard</code> for a two word server name on US region.
+                            <br>
+                            <code class="text-purple-300">!r r grim batol-eu</code> for a two word server name on EU
+                            region
                         </p>
                     </div>
                 </div>
             </div>
             <div class="flex relative pl-2 pb-20 sm:items-center">
-               <div class="h-full w-6 absolute inset-0 flex items-center justify-center hidden lg:flex">
+                <div class="h-full w-6 absolute inset-0 flex items-center justify-center hidden lg:flex">
                     <div class="h-full w-1 bg-gray-800 pointer-events-none"></div>
                 </div>
                 <div class="flex-grow md:pl-8 flex sm:items-center items-start flex-col sm:flex-row">
@@ -50,13 +56,15 @@
                 </div>
             </div>
             <div class="flex relative lg:pl-10 pb-20 sm:items-center w-full">
-               <div class="h-full w-6 absolute inset-0 flex items-center justify-center hidden lg:flex">
+                <div class="h-full w-6 absolute inset-0 flex items-center justify-center hidden lg:flex">
                     <div class="h-full w-1 bg-gray-800 pointer-events-none"></div>
                 </div>
-                <img src="!p.png" alt="example of !p call for akunda's bite to show the variety of information provided." class="md:pl-10 max-h-64">
+                <img src="!p.png"
+                     alt="example of !p call for akunda's bite to show the variety of information provided."
+                     class="md:pl-10 max-h-64">
             </div>
             <div class="flex relative pl-2 pb-10 sm:items-center">
-               <div class="h-full w-6 absolute inset-0 flex items-center justify-center hidden lg:flex">
+                <div class="h-full w-6 absolute inset-0 flex items-center justify-center hidden lg:flex">
                     <div class="h-full w-1 bg-gray-800 pointer-events-none"></div>
                 </div>
                 <div class="flex-grow md:pl-8 flex sm:items-center items-start flex-col sm:flex-row">
@@ -73,11 +81,43 @@
                 </div>
             </div>
             <div class="flex relative lg:pl-10 pb-20 sm:items-center w-full">
-               <div class="h-full w-6 absolute inset-0 flex items-center justify-center hidden lg:flex">
+                <div class="h-full w-6 absolute inset-0 flex items-center justify-center hidden lg:flex">
                     <div class="h-full w-1 bg-gray-800 pointer-events-none"></div>
                 </div>
-                <img src="bg+.png" alt="Example of !bg+ call for bountiful captain's feast specifically showing multi-level recipe behavior." class="md:pl-10 max-h-64">
+                <img src="bg+.png"
+                     alt="Example of !bg+ call for bountiful captain's feast specifically showing multi-level recipe behavior."
+                     class="md:pl-10 max-h-64">
+            </div>
+            <div class="flex relative pl-2 pb-20 sm:items-center">
+                <div class="h-full w-6 absolute inset-0 flex items-center justify-center hidden lg:flex">
+                    <div class="h-full w-1 bg-gray-800 pointer-events-none"></div>
+                </div>
+                <div class="flex-grow md:pl-8 flex sm:items-center items-start flex-col sm:flex-row">
+                    <div class="text-3xl flex-shrink-0 w-24 h-24 bg-gray-800 text-purple-400 rounded-full inline-flex items-center justify-center">
+                        extra
+                    </div>
+                    <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                        <h2 class="font-medium title-font text-white mb-1 text-xl">Other tips and tricks:</h2>
+                        <ul>
+                            <li><span class="italic">Shadowlands</span> legendary items can be searched by rank! Use <code>/rank #</code> for
+                                example
+                                <br>
+                                <code class="text-purple-300">!p shadowghast ring /rank 4</code>
+                                <br>
+                                You can even combine your favorite shorthand with the rank command such as <code
+                                        class="text-purple-300">!p
+                                    shadowghast r /rank 1</code>.
+                                <br>
+                            </li>
+                            <li>
+                                Two of our favorite short hand commands are <code class="text-purple-300">!bg+
+                                palat</code> and <code class="text-purple-300">!bg+ hedon</code> for <span class="italic">Shadowlands</span> feasts.
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
+
     </div>
 </section>
--- src/News.svelte
+++ src/News.svelte
@@ -5,8 +5,9 @@
 <div class="bg-gray-900 px-4 md:px-0">
     <div class="container mx-auto bg-blue-300 rounded-b-md p-4 text-gray-900">
         <div class="bg-blue-600 rounded-md px-3 py-2 mb-3 font-bold text-gray-200">
-            News 11/21/20
+            4/28/21
         </div>
-        Shadowlands items and recipes have been added to the bot. Please report any issues you see to the #bug-reports channel in discord.
+        Some vendor prices were updated, recipes like Codex of the Still Mind are
+        much more accurate now.
     </div>
 </div>
